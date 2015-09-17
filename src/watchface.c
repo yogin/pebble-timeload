ffef34075aad81982084d8f36d3cb457e4d964f6
Anthony Powles <github@idreamz.net>
2015-09-17 14:51:14 -0700

    Starting to add battery and timeline support

--- src/watchface.c
+++ src/watchface.c
@@ -2,11 +2,16 @@
 
 static int screen_width = 144;
 static int screen_height = 168;
+static int border = 1;
 
 static Window *s_main_window;
 static Layer *s_main_layer;
 static int hour_idx;
 static int minute_idx;
+
+static bool show_battery = true;
+static bool show_timeline = true;
+static int sidebar_width = 10;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -22,7 +27,6 @@ static void update_time() {
   layer_mark_dirty(s_main_layer);
 }
 
-
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
@@ -31,17 +35,35 @@ static void draw_time(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "bounds size: %dx%d", bounds.size.w, bounds.size.h);
 
-  int block_border = 1;
-  int block_spacer = 2 * block_border;
-  int block_width = (bounds.size.w - (2*block_border) - block_spacer) / 2;
-  int block_height = (bounds.size.h - (2*block_border) - (11*block_spacer)) / 12;
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "block %dx%d border:%d spacer:%d", block_width, block_height, block_border, block_spacer);
+  int block_spacer = 2 * border;
+
+  int time_area_width = bounds.size.w;
+  int time_area_height = bounds.size.h;
+  
+  if (show_battery) {
+    time_area_width -= sidebar_width + border;
+  }
+
+  if (show_timeline) {
+    time_area_width -= sidebar_width + 2 * border;
+  }
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "time area size: %dx%d", time_area_width, time_area_height);
+  
+  int block_width = (time_area_width - (2 * border) - block_spacer) / 2;
+  int block_height = (time_area_height - (2 * border) - (11*block_spacer)) / 12;
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "block %dx%d border:%d spacer:%d", block_width, block_height, border, block_spacer);
 
   graphics_context_set_stroke_color(ctx, GColorBlack);
   graphics_context_set_fill_color(ctx, GColorBlack);
 
+  int offset_x = border;
+  if (show_battery) {
+      offset_x += sidebar_width + block_spacer;
+  }
+  
   // draw hour blocks
-  for (int i = 0, x1 = block_border, x2 = block_border + block_width + block_spacer, start_y = block_border; i < 12; i++) {
+  for (int i = 0, x1 = offset_x, x2 = offset_x + block_width + block_spacer, start_y = border; i < 12; i++) {
     // left column
     GRect left_rect = GRect(x1, start_y, block_width, block_height);
     if (12-i <= hour_idx) {
@@ -62,15 +84,25 @@ static void draw_time(Layer *layer, GContext *ctx) {
 
     // move down
     start_y += block_height + block_spacer;
-  }
+  }  
 }
 
-static void main_window_load(Window *window) {
-  current_hour_idx = (int*)malloc(sizeof(int));
-  current_minute_idx = (int*)malloc(sizeof(int));
+static void draw_battery(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  GRect battery_rect = GRect(border, border, sidebar_width, bounds.size.h - 2 * border);
+  graphics_draw_round_rect(ctx, battery_rect, 2);  
+}
+
+static void draw_timeline(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  GRect timeline_rect = GRect(bounds.size.w - sidebar_width - border, border, sidebar_width, bounds.size.h - 2 * border);
+  graphics_draw_round_rect(ctx, timeline_rect, 2);  
+}
 
 static void main_layer_update_proc(Layer *layer, GContext *ctx) {
   draw_time(layer, ctx);
+  draw_battery(layer, ctx);
+  draw_timeline(layer, ctx);
 }
 
 static void main_window_load(Window *window) {
