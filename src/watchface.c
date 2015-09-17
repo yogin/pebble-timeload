eef7bf1c648072434269afd38ce8e3e395ca719a
Anthony Powles <github@idreamz.net>
2015-09-17 14:50:00 -0700

    Get rid of unnecessary pointers

--- src/watchface.c
+++ src/watchface.c
@@ -5,8 +5,8 @@ static int screen_height = 168;
 
 static Window *s_main_window;
 static Layer *s_main_layer;
-static int *current_hour_idx;
-static int *current_minute_idx;
+static int hour_idx;
+static int minute_idx;
 
 static void update_time() {
   time_t temp = time(NULL);
@@ -14,11 +14,11 @@ static void update_time() {
 
   // each hour_index represents 1 hour
   // convert hours from 24h format to 12h format
-  *current_hour_idx = tick_time->tm_hour > 12 ? tick_time->tm_hour - 12 : tick_time->tm_hour;
+  hour_idx = tick_time->tm_hour > 12 ? tick_time->tm_hour - 12 : tick_time->tm_hour;
   // each minute_index represents 5 minutes
-  *current_minute_idx = tick_time->tm_min / 5; 
+  minute_idx = tick_time->tm_min / 5; 
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "updated time %d:%d into hour_idx:%d minute_idx:%d", tick_time->tm_hour, tick_time->tm_min, *current_hour_idx, *current_minute_idx);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "updated time %d:%d into hour_idx:%d minute_idx:%d", tick_time->tm_hour, tick_time->tm_min, hour_idx, minute_idx);
   layer_mark_dirty(s_main_layer);
 }
 
@@ -27,7 +27,7 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 }
 
-static void main_layer_update_proc(Layer *layer, GContext *ctx) {
+static void draw_time(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "bounds size: %dx%d", bounds.size.w, bounds.size.h);
 
@@ -43,19 +43,21 @@ static void main_layer_update_proc(Layer *layer, GContext *ctx) {
   // draw hour blocks
   for (int i = 0, x1 = block_border, x2 = block_border + block_width + block_spacer, start_y = block_border; i < 12; i++) {
     // left column
-    if (12-i <= *current_hour_idx) {
-      graphics_fill_rect(ctx, GRect(x1, start_y, block_width, block_height), 2, GCornersAll);
+    GRect left_rect = GRect(x1, start_y, block_width, block_height);
+    if (12-i <= hour_idx) {
+      graphics_fill_rect(ctx, left_rect, 2, GCornersAll);
     }
     else {
-      graphics_draw_round_rect(ctx, GRect(x1, start_y, block_width, block_height), 2);
+      graphics_draw_round_rect(ctx, left_rect, 2);
     }
 
     // right column
-    if (12-i <= *current_minute_idx) {
-      graphics_fill_rect(ctx, GRect(x2, start_y, block_width, block_height), 2, GCornersAll);
+    GRect right_rect = GRect(x2, start_y, block_width, block_height);
+    if (12-i <= minute_idx) {
+      graphics_fill_rect(ctx, right_rect, 2, GCornersAll);
     }
     else {
-      graphics_draw_round_rect(ctx, GRect(x2, start_y, block_width, block_height), 2);  
+      graphics_draw_round_rect(ctx, right_rect, 2);  
     }
 
     // move down
@@ -67,6 +69,11 @@ static void main_window_load(Window *window) {
   current_hour_idx = (int*)malloc(sizeof(int));
   current_minute_idx = (int*)malloc(sizeof(int));
 
+static void main_layer_update_proc(Layer *layer, GContext *ctx) {
+  draw_time(layer, ctx);
+}
+
+static void main_window_load(Window *window) {
   s_main_layer = layer_create(GRect(0, 0, screen_width, screen_height));
   layer_set_update_proc(s_main_layer, main_layer_update_proc);
   layer_add_child(window_get_root_layer(window), s_main_layer);
@@ -76,8 +83,6 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   layer_destroy(s_main_layer);
-  free(current_hour_idx);
-  free(current_minute_idx);
 }
 
 static void init() {
